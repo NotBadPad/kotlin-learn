6da7f1b98f960c2a8c3bc7cd2ffbfccc4add8e17
NotBadPad <awayy1432@163.com>
2017-05-27 11:39:50 +0800

    rebuild

---
+++ src/main/kotlin/com/goodtemp/learn/Hello.kt
@@ -0,0 +1,6 @@
+package com.goodtemp.learn
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/goodtemp/learn/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.goodtemp.learn
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
