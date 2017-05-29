b0bd2662e1588899294e5c7cf73d8f7d7772c2c1
NotBadPad <awayy1432@163.com>
2017-05-29 11:44:23 +0800

    class done

--- src/main/kotlin/com/goodtemp/learn/ClassLearn.kt
+++ src/main/kotlin/com/goodtemp/learn/ClassLearn.kt
@@ -23,9 +23,42 @@ class Customer(name: String, var age: Int) : Base(name){
     }
 }
 
+data class User(var name:String, var age:Int) {
+    constructor(name:String) :this(name, 10)
+}
+
+
+interface Base1 {
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base1 {
+    override fun print() { println(x) }
+}
+
+class Derived(b: Base1) : Base1 by b{
+    fun print(b:Base1) {
+        println()
+        println("aaaa")
+    }
+}
+
+
 fun main(args: Array<String>) {
+    //属性
     var cu = Customer("aa", 10, "aaa")
     cu.age =11
     cu.other = "ccc"
     cu.print()
+
+    //数据类
+    val user = User("gj", 10)
+    println(user.toString())
+
+    //委托
+    var bi = BaseImpl(10)
+    Derived(bi).print()
+    var d = Derived(bi)
+    d.print()
+    d.print(bi)
 }
